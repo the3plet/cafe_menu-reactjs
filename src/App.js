cd73f554e354ed03e7a88ce99f8632f86a2ef730
the3plet <soorajp2002@gmail.com>
2022-10-08 22:22:04 +0530

    Added Footer
    Credits Footer added + Minor changes

--- src/App.js
+++ src/App.js
@@ -4,40 +4,46 @@ function App() {
   return (
     <Fragment>
       <h5 className={hi.menu}>
-        <span className={hi.border}>THE MENU</span>
+        <span className={hi.border}> μmenu</span>
       </h5>
 
       <div className={hi.box}>Food Items</div>
 
       <div className={hi.mainbox}>
         <div>
-          <h5 className={hi.h5}>Bread Basket</h5>
+          <h5 className={hi.h5}> μBread Basket</h5>
           <p className={hi.p}>
             Assortment of fresh baked fruit breads and muffins 5.50
           </p>
           <br></br>
 
-          <h5 className={hi.h5}>Honey Almond Granola with Fruits</h5>
+          <h5 className={hi.h5}>μHoney Almond Granola with Fruits</h5>
           <p className={hi.p}>
             Natural cereal of honey toasted oats, raisins, almonds and dates
             7.00
           </p>
           <br></br>
 
-          <h5 className={hi.h5}>Belgian Waffle</h5>
+          <h5 className={hi.h5}>μBelgian Waffle</h5>
           <p className={hi.p}>Vanilla flavored batter with malted flour 7.50</p>
           <br></br>
 
-          <h5 className={hi.h5}>Scrambled eggs</h5>
+          <h5 className={hi.h5}>μScrambled eggs</h5>
           <p className={hi.p}>
             Scrambled eggs, roasted red pepper and garlic, with green onions
             7.50
           </p>
           <br></br>
 
-          <h5 className={hi.h5}>Blueberry Pancakes</h5>
+          <h5 className={hi.h5}>μBlueberry Pancakes</h5>
           <p className={hi.p}>With syrup, butter and lots of berries 8.50</p>
         </div>
+      </div>
+
+      <div>
+        <footer className={hi.sooraj}>
+          <p>Made by Sooraj😉</p>
+        </footer>
       </div>
     </Fragment>
   );
